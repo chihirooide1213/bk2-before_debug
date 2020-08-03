a5fffde98590259d6c0e8dce569e1f52fbda0139
chihirooide1213 <donnakachi@gmail.com>
2020-08-03 11:53:58 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    @book.user_id = current_user.id
+     if @book.save
+      redirect_to book_path(@book), notice: "You have created book successfully."
+    else
+      render action: :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def delete
+    @book = Book.find(params[:id])
+    @book.destoy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
